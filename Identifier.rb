5e2c30d030ff4c6b0ebd5959bae7e27252a9438e
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-04-18 00:15:57 +0900

    課題ステータス、完了理由のIDを取得するメソッドを追加

--- Identifier.rb
+++ Identifier.rb
@@ -6,24 +6,39 @@ class Identifier
     @client = client
   end
 
-  def projectId(projectKey)
+  def project(projectKey)
     project = @client.get_projects.body.find do |pb|
       pb.projectKey == projectKey
     end
     project.id
   end
 
-  def priorities(label)
+  def priority(label)
     priority = @client.get_priorities.body.find do |pr|
       pr.name == label
     end
     priority.id
   end
 
-  def issueTypeId(project_key, label)
+  def issueType(project_key, label)
     type = @client.get_issue_types(project_key).body.find do |it|
       it.name == label
     end
     type.id
   end
+
+  def status(label)
+    status = @client.get_statuses.body.find do |st|
+      st.name == label
+    end
+    status.id
+  end
+
+  def resolution(label)
+    resolution = @client.get_resolutions.body.find do |res|
+      res.name == label
+    end
+    resolution.id
+  end
+
 end
