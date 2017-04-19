3fb5133871e746fe881b7e2c6a1ce3672b33aa74
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-04-19 22:35:28 +0900

    課題を作成するメソッドと課題をクローズするメソッドを実装

--- Backlog.rb
+++ Backlog.rb
@@ -7,14 +7,14 @@ class Backlog
     @client = client
     @identifier = Identifier.new(client)
     @projectId = @identifier.project(projectName)
-    @userId = @identifier.user(userName)
+    @userName = userName
   end
 
   # 自信が担当になっており、完了していない課題の一覧を取得
   def showIncompleteMyIssues
     issues = @client.get_issues({
       :projectId => [@projectId],
-      :assigneeId => [@userId],
+      :assigneeId => [@identifier.user(@userName)],
       :statusId => [
         @identifier.status('未対応'),
         @identifier.status('処理中'),
@@ -22,6 +22,27 @@ class Backlog
       ]
     })
     issues.body
+  end
+
+  # 課題を新規登録する
+  def createIssue(summary, description, type, category, user = @userName)
+    @client.create_issue(summary, {
+      :projectId => @projectId,
+      :description => description,
+      :issueTypeId => @identifier.issueType(@projectId, type),
+      :categoryId => [@identifier.category(@projectId, category)],
+      :priorityId => @identifier.priority('中'),
+      :assigneeId => @identifier.user(user),
+    })
+  end
+
+  # 課題をクローズする
+  def closeIssue(key, hour, comment = '')
+    @client.update_issue(key, {
+      :statusId => @identifier.status('完了'),
+      :resolutionId => @identifier.resolution('対応済み'),
+      :actualHours => hour,
+    })
   end
 
 end
